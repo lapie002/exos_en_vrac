ab4b733ed31835822a45b521d37c8b5132e67466
Bruno <lapierre.bruno@gmail.com>
2018-03-12 04:09:20 +0100

    commit exo 7

--- src/exos_en_vrac/Exos_en_vrac.java
+++ src/exos_en_vrac/Exos_en_vrac.java
@@ -19,12 +19,16 @@ public class Exos_en_vrac {
     public static void main(String[] args) {
         // TODO code application logic here
         
-//          Exercice1.launch();
+//        Exercice1.launch();
 //        Exercice2.launch();
 //        Exercice3.launch();
 //        Exercice4.launch();
 //        Exercice5.launch();
-          Exercice6.launch();
+//        Exercice6.launch();
+        Exercice7.launch();
+        
+      
+/* partie 2 exo sur la POO Personne*/        
         
 //        Pays france = new Pays("France");
 //        Pays belgique = new Pays("Belgique");
